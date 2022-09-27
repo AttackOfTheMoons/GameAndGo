82b046abe18f8a37e965485c601ca3a2e613abc2
Theo <purplecatfishz@aol.com>
2022-09-27 17:01:25 -0400

    reinitialized the spring project

---
+++ src/test/java/com/uncg/gameandgo/GameAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncg.gameandgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAndGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
