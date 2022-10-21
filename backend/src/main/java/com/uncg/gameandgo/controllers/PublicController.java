130c9285b56168cd90d826aa4063836bb0ccdcb1
AttackOfTheMoons <42690834+AttackOfTheMoons@users.noreply.github.com>
2022-10-20 22:57:35 -0400

    bug / formatting fixes

--- backend/src/main/java/com/uncg/gameandgo/controllers/PublicController.java
+++ backend/src/main/java/com/uncg/gameandgo/controllers/PublicController.java
@@ -1,9 +1,7 @@
 package com.uncg.gameandgo.controllers;
 
-import com.uncg.gameandgo.repository.UserRepository;
 import java.io.IOException;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
